cce602d46d3699f0f45728efeb7794f11b170043
WAN,Ji <wanji@live.com>
2015-04-04 23:43:52 +0800

    add Profiler

--- src/util.py
+++ src/util.py
@@ -24,6 +24,9 @@ import cv2
 from distance import distFunc
 import bottleneck
 
+# profiling
+import time
+
 
 """
 Different options for kmeans.
@@ -67,3 +70,95 @@ def pq_knn(dist, topk):
     ids = bottleneck.argpartsort(dist, topk)[:topk]
     ids = ids[dist[ids].argsort()]
     return ids
+
+
+# Profiling
+
+class Profiler(object):
+    """ Profiling the running time of code snippet.
+    """
+    class Record(object):
+        __slots__ = ["name", "time", "count", "t0"]
+
+        def __init__(self, name):
+            self.name = name
+            self.reset()
+
+        def reset(self):
+            self.time = 0.0
+            self.count = 0
+            self.t0 = None
+
+        def start(self):
+            self.t0 = time.time()
+
+        def end(self):
+            self.time += (time.time() - self.t0)
+            self.count += 1
+            self.t0 = None
+
+        def average(self):
+            return self.time / self.count if self.count > 0 else 0
+
+    __slots__ = ["records",
+                 "cur_record",
+                 "name_stack"]
+
+    def __init__(self):
+        self.reset()
+
+    def start(self, name):
+        """
+        Start the timer.
+        `name` is the description of the current code snippet.
+        """
+        if name not in self.records:
+            self.records[name] = Profiler.Record(name)
+        self.cur_record = self.records[name]
+        self.name_stack.append(name)
+        self.cur_record.start()
+
+    def end(self, name=None):
+        """
+        Calculate the time costs of the current code snippet.
+        """
+        if name is not None and name != self.name_stack[-1]:
+            raise Exception("name '%s' should be '%s'" %
+                            (name, self.name_stack[-1]))
+        self.cur_record.end()
+        self.name_stack.pop()
+
+    def sum_overall(self):
+        """
+        Return the sum of overall time costs for each code snippet.
+        """
+        return sum([rec.time for name, rec in self.records.iteritems()])
+
+    def sum_average(self):
+        """
+        Return the sum of average time costs for each code snippet.
+        """
+        return sum([rec.average() for name, rec in self.records.iteritems()])
+
+    def str_overall(self, fmt="%s: %.4fs"):
+        """
+        Return the overall time costs for each code snippet as string.
+        """
+
+        return ";\t".join([fmt % (name, rec.time)
+                           for name, rec in self.records.iteritems()])
+
+    def str_average(self, fmt="%s: %.4f"):
+        """
+        Return the average time costs for each code snippet as string.
+        """
+        return ";\t".join([fmt % (name, rec.average())
+                           for name, rec in self.records.iteritems()])
+
+    def reset(self):
+        """
+        Reset the time costs and counters.
+        """
+        self.records = {}
+        self.name_stack = []
+        self.cur_record = None
